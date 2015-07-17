8d7c8651973afdd1d70184209b98d158d8c31f0d
vitogit <apuntalorapido@gmail.com>
2015-07-17 13:36:35 -0300

    change timezone

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module ListingScraper
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.time_zone = 'Montevideo'
+
   end
 end
