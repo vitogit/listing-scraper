a22622fbc35806806c38fd8e3206d47d1f9af35e
vitogit <apuntalorapido@gmail.com>
2015-08-02 21:52:29 -0300

    add negative search

--- app/assets/javascripts/listing.js
+++ app/assets/javascripts/listing.js
@@ -8,9 +8,20 @@
 $(document).on('page:change', function () {
     var table = $('.datatable').DataTable({
       "order": [[ 8, "desc" ]],
-      "pageLength": 50
+      "pageLength": 50,
 
     });
+
+    // $("div.negative_search").html('Negative Search:<input class="form-control input-sm" type="text" id="negative_search">');
+    $('#negative_search').keyup(function(){
+      if ($(this).val() == "") {
+          table.search($(this).val()).draw();
+      } else {
+        table.search( '^(?!.*'+$(this).val()+').*$', true, false ).draw();
+        $('.input-sm').val('');
+      }
+
+    })
 
     // show spinner on AJAX start
     $(document).ajaxStart(function(){
